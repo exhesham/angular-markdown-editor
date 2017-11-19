59775d3aebbffd3d6c96712f767d229a737c2c2e
exhesham <exhesham@gmail.com>
2017-11-19 23:09:58 +0200

    bold work

--- src/app/editor.component.spec.ts
+++ src/app/editor.component.spec.ts
@@ -1,30 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 
-import { ToolbarComponent } from './toolbar.component';
+import { EditorComponent } from './editor.component';
 
 describe('ToolbarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        ToolbarComponent
+        EditorComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(ToolbarComponent);
+    const fixture = TestBed.createComponent(EditorComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(ToolbarComponent);
+    const fixture = TestBed.createComponent(EditorComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(ToolbarComponent);
+    const fixture = TestBed.createComponent(EditorComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
--- src/app/editor.component.ts
+++ src/app/editor.component.ts
@@ -2,10 +2,19 @@
  * Created by hishamy on 16/11/2017.
  */
 import {
-  AfterViewInit, Component, ElementRef, EmbeddedViewRef, EventEmitter, Output, Renderer, Renderer2, ViewChild,
+  AfterViewInit, Component, ElementRef, Renderer2, ViewChild,
   ViewContainerRef
 } from '@angular/core';
-import {ViewData} from "@angular/core/src/view";
+
+
+enum ElementType{
+  p,
+  span,
+  bold,
+  italic,
+  quote,
+  underline,
+}
 
 @Component({
   selector: 'app-editor',
@@ -13,66 +22,59 @@ import {ViewData} from "@angular/core/src/view";
   styleUrls: ['./editor.component.css']
 
 })
+
 export class EditorComponent  implements AfterViewInit {
+
 
   @ViewChild('richtextbox', {read: ViewContainerRef}) richtextbox: ViewContainerRef;
   @ViewChild('p_dom') p_dom;
   @ViewChild('abcd')
   private abcd: ElementRef;
+  private last_focused_element: any;
 
-  constructor(private renderer : Renderer2){}
-  p: any;
+  constructor(private renderer : Renderer2){
+
+  }
+
   ngAfterViewInit(): void {
     this.renderer.parentNode(this.richtextbox)
+    let p = this.inject_new_element(ElementType.p, 'I am a text', this.abcd.nativeElement);
+    p.focus()
 
-    this.p = this.renderer.createElement( 'p')
-    const text = this.renderer.createText('Click here to add li');
-    this.renderer.appendChild(this.p, text);
-    this.renderer.appendChild(this.abcd.nativeElement, this.p);
-    this.renderer.setAttribute(this.p, 'contenteditable','true')
-    this.renderer.listen(this.p, 'keypress', this.keypress_decorator(this.p));
-    this.p.focus()
-
-    //p.setElementAttribute(null, 'contenteditable','true')
-    // this.renderer.setProperty(p.nativeElement,)
-
-    //
-    //this.richtextbox.insert(p);
-
+    console.log(this.get_focused_element())
   }
-  editor_keypress(e) {
+  get_focused_element(): any {
+    console.log(this.last_focused_element)
+    return this.last_focused_element
   }
-
   apply_bold(event){
-    let [startOffset, lastOffset] = this.get_caret_position()
-    
+    let focused_element = this.get_focused_element();
 
+    let [start_offset, last_offset] = this.get_caret_position(focused_element);    // get the selected text
+    let native_val = focused_element.innerText;  // get the text itself
+    let left_val = native_val.substring(0,start_offset);   // need to split the string
+    let right_val = last_offset <= native_val.length? native_val.substring(last_offset) : '';  // empty string if the caret is at the end of the element
+    let parent = focused_element.parentElement;
+    console.log('start_offset, last_offset:',start_offset, last_offset)
+    console.log('native_val:',native_val)
+    console.log('left_val:',left_val)
+    console.log('right_val:',right_val)
+    console.log('parent of focused:',parent)
+    let new_wrapper_child = this.inject_new_element(ElementType.p, '',parent)
+    let selected_substring = native_val.substring(start_offset, last_offset);
+    let left_element = this.inject_new_element(ElementType.span,left_val, new_wrapper_child);   // put the right substring in a new element right to the bold
+    let bold = this.inject_new_element(ElementType.bold,selected_substring, new_wrapper_child );   // create bold dom
+    //TODO: the new right element type should be the same as the focused_element
+    let right_element = this.inject_new_element(ElementType.span,right_val, new_wrapper_child );   // put the right substring in a new element right to the bold
+    this.renderer.removeChild(focused_element.parentElement, focused_element)
+    bold.focus();
   }
-  get_caret_position(){
-    console.log('applying bold')
-
-    var doc = this.p.ownerDocument || this.p.document;
-    var win = doc.defaultView || doc.parentWindow;
+  get_caret_position(doc: any){
+    var win = window;
     var caretOffset = 0;
     var sel = win.getSelection();
-    if (sel.rangeCount > 0) {
-      var range = win.getSelection().getRangeAt(0);
-      var preCaretRange = range.cloneRange();
-      preCaretRange.selectNodeContents(this.p);
-      preCaretRange.setEnd(range.endContainer, range.endOffset);
-      caretOffset = preCaretRange.toString().length;
-    } else if ((sel = doc.selection) && sel.type != "Control") {
-      var textRange = sel.createRange();
-      var preCaretTextRange = doc.body.createTextRange();
-      preCaretTextRange.moveToElementText(this.p);
-      preCaretTextRange.setEndPoint("EndToEnd", textRange);
-      caretOffset = preCaretTextRange.text.length;
-    }
-
-    console.log(caretOffset)    //this is the caret offset
-    console.log(sel.anchorOffset) // this is the start of the selection - if equal to offset then no selection
-    this.p.focus()
-    return [sel.anchorOffset, caretOffset]
+    doc.focus()
+    return [Math.min(sel.focusOffset,sel.anchorOffset), Math.max(sel.focusOffset,sel.anchorOffset)]
   }
 
 
@@ -83,14 +85,38 @@ export class EditorComponent  implements AfterViewInit {
 
   private keypress_decorator(p: any) {
     return (event) =>{
-      if(event.key === 'b'){
-        // console.log(event.target.innerText)
-        // const text = this.renderer.createText('Click here to add li 2');
-        // let p2 = this.renderer.createElement( 'p')
-        // this.renderer.listen(p2, 'keypress', this.keypress_decorator(p2));
-        // this.renderer.appendChild(p2, text);
-        // p.appendChild(p2)
+      if(event.key === 'Enter'){
       }
     }
+  }
+  private focus_decorator(p: any) {
+    return (event) =>{
+      this.last_focused_element = p
+    }
+  }
+
+  private inject_new_element(element_type: ElementType, text: string, parent: any) {
+    let name = 'p';
+    switch (element_type){
+      case ElementType.bold:
+        name = 'b';
+        break;
+      case ElementType.italic:
+        name = 'i';
+        break;
+      case ElementType.span:
+        name = 'span';
+        break;
+    }
+    let new_element = this.renderer.createElement(name)
+
+    const text_element = this.renderer.createText(text);
+    this.renderer.appendChild(new_element, text_element);
+    this.renderer.appendChild(parent, new_element);
+    this.renderer.setAttribute(new_element, 'contenteditable','true')
+    this.renderer.listen(new_element, 'keypress', this.keypress_decorator(new_element));
+    this.renderer.listen(new_element, 'focus', this.focus_decorator(new_element));
+
+    return new_element;
   }
 }
