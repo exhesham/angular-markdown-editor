950c9ae288d08f757e902477b43d1c7033ad0c02
exhesham <exhesham@gmail.com>
2017-11-20 00:20:01 +0200

    still some issues...

--- src/app/editor.component.ts
+++ src/app/editor.component.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core';
 
 
-enum ElementType{
+enum ElementType {
   p,
   span,
   bold,
@@ -23,7 +23,7 @@ enum ElementType{
 
 })
 
-export class EditorComponent  implements AfterViewInit {
+export class EditorComponent implements AfterViewInit {
 
 
   @ViewChild('richtextbox', {read: ViewContainerRef}) richtextbox: ViewContainerRef;
@@ -32,7 +32,7 @@ export class EditorComponent  implements AfterViewInit {
   private abcd: ElementRef;
   private last_focused_element: any;
 
-  constructor(private renderer : Renderer2){
+  constructor(private renderer: Renderer2) {
 
   }
 
@@ -41,65 +41,84 @@ export class EditorComponent  implements AfterViewInit {
     let p = this.inject_new_element(ElementType.p, 'I am a text', this.abcd.nativeElement);
     p.focus()
 
-    console.log(this.get_focused_element())
+
   }
+
   get_focused_element(): any {
     console.log(this.last_focused_element)
     return this.last_focused_element
   }
-  apply_bold(event){
+
+  apply_bold(event, focusable) {
     let focused_element = window.getSelection().focusNode;
     let parent = focused_element.parentElement;
+    let ancestor = focused_element.parentElement.parentElement;
     let [start_offset, last_offset] = this.get_caret_position();    // get the selected text
     let native_val = window.getSelection().focusNode.nodeValue;  // get the text itself
+    let left_val = native_val.substring(0, start_offset);   // need to split the string
+    let right_val = last_offset <= native_val.length ? native_val.substring(last_offset) : '';  // empty string if the caret is at the end of the element
+
+    console.log('focused_element:',focused_element)
+    console.log('parent:',parent)
+    console.log('ancestor:',ancestor)
     console.log('native_val:',native_val)
-    let left_val = native_val.substring(0,start_offset);   // need to split the string
-    let right_val = last_offset <= native_val.length? native_val.substring(last_offset) : '';  // empty string if the caret is at the end of the element
-
-    console.log('start_offset, last_offset:',start_offset, last_offset)
-
     console.log('left_val:',left_val)
     console.log('right_val:',right_val)
-    console.log('focused:',focused_element)
-    console.log('parent of focused:',parent)
+
     let new_wrapper_child = parent
     let selected_substring = native_val.substring(start_offset, last_offset);
-    let left_element = this.inject_new_element(ElementType.span,left_val, new_wrapper_child);   // put the right substring in a new element right to the bold
-    let bold = this.inject_new_element(ElementType.bold,selected_substring, new_wrapper_child );   // create bold dom
-    //TODO: the new right element type should be the same as the focused_element
-    let right_element = this.inject_new_element(ElementType.span,right_val, new_wrapper_child );   // put the right substring in a new element right to the bold
-    this.renderer.removeChild(focused_element.parentElement, focused_element)
+    if (left_val) {
+      this.inject_new_element(ElementType.span, left_val, new_wrapper_child);   // put the right substring in a new element right to the bold
+    }
+
+    let bold
+    if (parent.tagName.toLowerCase() == 'b') {
+      console.log('will unbold')
+      bold = this.inject_new_element(ElementType.span, selected_substring, new_wrapper_child.parentElement);   // create bold dom
+    } else {
+      bold = this.inject_new_element(ElementType.bold, selected_substring, new_wrapper_child);   // create bold dom
+    }
     bold.focus();
+    if (right_val) {
+      this.inject_new_element(ElementType.span, right_val, new_wrapper_child);   // put the right substring in a new element right to the bold
+    }
+
+    this.renderer.removeChild(focused_element.parentElement, focused_element)
+    console.log('will focus on ', bold)
+
+
   }
-  get_caret_position(){
+
+  get_caret_position() {
     var win = window;
     var caretOffset = 0;
     var sel = win.getSelection();
 
-    return [Math.min(sel.focusOffset,sel.anchorOffset), Math.max(sel.focusOffset,sel.anchorOffset)]
+    return [Math.min(sel.focusOffset, sel.anchorOffset), Math.max(sel.focusOffset, sel.anchorOffset)]
   }
 
 
   private view_keypress(view1) {
     console.log('i am a view:', view1.elementRef);
-    let p = this.renderer.createElement( 'p')
+    let p = this.renderer.createElement('p')
   }
 
   private keypress_decorator(p: any) {
-    return (event) =>{
-      if(event.key === 'Enter'){
+    return (event) => {
+      if (event.key === 'Enter') {
       }
     }
   }
-  private focus_decorator(p: any) {
-    return (event) =>{
-      this.last_focused_element = p
+
+  private click_decorator(p: any) {
+    return (event) => {
+      p.focus()
     }
   }
 
   private inject_new_element(element_type: ElementType, text: string, parent: any) {
     let name = 'p';
-    switch (element_type){
+    switch (element_type) {
       case ElementType.bold:
         name = 'b';
         break;
@@ -115,10 +134,11 @@ export class EditorComponent  implements AfterViewInit {
     const text_element = this.renderer.createText(text);
     this.renderer.appendChild(new_element, text_element);
     this.renderer.appendChild(parent, new_element);
-    this.renderer.setAttribute(new_element, 'contenteditable','true')
+    this.renderer.setAttribute(new_element, 'contenteditable', 'true')
     this.renderer.listen(new_element, 'keypress', this.keypress_decorator(new_element));
-    this.renderer.listen(new_element, 'focus', this.focus_decorator(new_element));
-
+    this.renderer.listen(new_element, 'click', this.click_decorator(new_element));
+    // this.renderer.listen(new_element, 'focus', this.focus_decorator(new_element));
+    new_element.focus()
     return new_element;
   }
 }
