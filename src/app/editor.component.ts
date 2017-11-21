049e6c9ea8d685226d532e7b20506a3cc2f864a1
hisham yasin <hisham.yasin@il.ibm.com>
2017-11-21 14:03:59 +0200

    irun command

--- src/app/editor.component.ts
+++ src/app/editor.component.ts
@@ -73,81 +73,17 @@ export class EditorComponent implements AfterViewInit {
       text, ranges];
   }
 
-  apply_bold(event, focusable) {
-
-    var sel;
-    if (window.getSelection) {
-      sel = window.getSelection()
-
-    } else if (document.getSelection() && document.getSelection().type != "Control") {
-      sel = document.getSelection()
-    }
-    var selected_text = sel.toString();
-    let focused_element = this.get_focused_element()
-    // handle extent
-    let range = sel.getRangeAt(0)
-    console.log('range=', range)
-    let left_str, right_str
-    if (range.startContainer  != range.endContainer) {
-      left_str = range.startContainer.data.substring(0, range.startOffset )
-      right_str = range.endContainer.data.substring(range.endOffset)
-    }else{
-      left_str = range.startContainer.data.substring(0, range.startOffset)
-      right_str = range.startContainer.data.substring(range.endOffset)
-    }
-    let parent = sel.baseNode.parentElement
-    console.log('parent=', parent)
-    console.log('selected_text=', selected_text)
-    console.log('left_str=', left_str)
-    console.log('right_str=', right_str)
-    let left_node = this.inject_new_element(ElementType.span, left_str,parent)
-    let bold_node = this.inject_new_element(ElementType.bold, selected_text,parent)
-    let right_node = this.inject_new_element(ElementType.span, right_str,parent)
-    this.renderer.removeChild(focused_element.parentElement, focused_element)
-
-
-
-
-
-    //this.renderer.removeChild(focused_element.parentElement, focused_element)
-    // console.log('get_selection_text():', this.get_selection_text())
-    // let focused_element = window.getSelection().focusNode;
-    // let parent = focused_element.parentElement;
-    // let ancestor = focused_element.parentElement.parentElement;
-    // let [start_offset, last_offset] = this.get_caret_position();    // get the selected text
-    // let native_val = window.getSelection().focusNode.nodeValue;  // get the text itself
-    // let left_val = native_val.substring(0, start_offset);   // need to split the string
-    // let right_val = last_offset <= native_val.length ? native_val.substring(last_offset) : '';  // empty string if the caret is at the end of the element
-    //
-    // console.log('focused_element:', focused_element)
-    // console.log('parent:', parent)
-    // console.log('ancestor:', ancestor)
-    // console.log('native_val:', native_val)
-    // console.log('left_val:', left_val)
-    // console.log('right_val:', right_val)
-    //
-    // let new_wrapper_child = parent
-    // let selected_substring = native_val.substring(start_offset, last_offset);
-    // if (left_val) {
-    //   this.inject_new_element(ElementType.span, left_val, new_wrapper_child);   // put the right substring in a new element right to the bold
-    // }
-    //
-    // let bold
-    // if (parent.tagName.toLowerCase() == 'b') {
-    //   console.log('will unbold')
-    //   bold = this.inject_new_element(ElementType.span, selected_substring, new_wrapper_child.parentElement);   // create bold dom
-    // } else {
-    //   bold = this.inject_new_element(ElementType.bold, selected_substring, new_wrapper_child);   // create bold dom
-    // }
-    // bold.focus();
-    // if (right_val) {
-    //   this.inject_new_element(ElementType.span, right_val, new_wrapper_child);   // put the right substring in a new element right to the bold
-    // }
-    //
-    // this.renderer.removeChild(focused_element.parentElement, focused_element)
-    // console.log('will focus on ', bold)
-
-
+  apply_bold(event) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+    document.execCommand('bold');
+  }
+  apply_italic(event) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+    document.execCommand('italic');
+  }
+  apply_underline(event) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+    document.execCommand('underline');
   }
 
   get_caret_position() {
